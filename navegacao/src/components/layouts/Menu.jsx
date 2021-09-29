86afc0166aec6f1b731ca79b7a1d2c679e624820
Roberto Maciel <rob.nettom@gmail.com>
2021-09-28 22:43:00 -0300

    Finalizado Módulo 4 - Navegação com React Router

---
+++ navegacao/src/components/layouts/Menu.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { Link } from "react-router-dom";
+import "./Menu.css"
+
+const Menu = props => (
+    <div className="Menu">
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">
+                        Início
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/param/123">
+                        Param #01
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/param/legal">
+                        Param #02
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/about">
+                        Sobre
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/naoExiste">
+                        Não Existe
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+)
+
+export default Menu;
